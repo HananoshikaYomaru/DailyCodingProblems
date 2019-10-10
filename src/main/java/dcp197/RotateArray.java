48972bc6e62413433c58d370354c4c7f7c800518
Yomaru <mlkyeung@connect.ust.hk>
2019-10-11 01:12:36 +0800

    daily coding problem #197

---
+++ src/main/java/dcp197/RotateArray.java
@@ -0,0 +1,17 @@
+package dcp197;
+
+public class RotateArray {
+	public static int[] rotateRight(int[] array, int k ) {
+		int[] newArray = new int[array.length] ; 
+		for(int i = 0 ; i < array.length; i++)
+			newArray[(i + k) % array.length ] = array[i] ; 
+		return newArray; 
+	}
+	
+	public static int[] rotateLeft(int[] array, int k ) {
+		int[] newArray = new int[array.length] ; 
+		for(int i = 0 ; i < array.length; i++)
+			newArray[(i - k) % array.length ] = array[i] ; 
+		return newArray; 
+	}
+}
