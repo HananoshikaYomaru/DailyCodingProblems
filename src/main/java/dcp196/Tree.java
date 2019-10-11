8a9fc12927100ba6a4b9e4c4c24380af4b41b516
Yomaru <mlkyeung@connect.ust.hk>
2019-10-11 09:48:45 +0800

    add #196

---
+++ src/main/java/dcp196/Tree.java
@@ -0,0 +1,30 @@
+package dcp196;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tree{
+	int size = 0; // true size without counting disabled
+	List<Node> nodes = new ArrayList<>(); 
+	
+	
+	public void add(int num) {
+		this.nodes.add(new Node(num)) ; 
+		size++ ; 
+	}
+	public void add() {
+		this.nodes.add(new Node(0, true)) ;  
+	}
+	
+	public void add(Node node) {
+		this.nodes.add(node) ; 
+		if(!node.disable)
+			size++;
+	}
+	
+	public void printTree(){
+		for(Node n : nodes)
+			System.out.println(n.num + " : " + n.disable) ; 
+		
+	}
+}
---
+++ src/test/java/dcp196/MostFrequentSumTest.java
@@ -0,0 +1,40 @@
+package dcp196;
+
+//import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MostFrequentSumTest {
+
+	@Test
+	public void testFind() {
+		Tree tree = new Tree();
+		tree.add(5);
+		tree.add(2);
+		tree.add(-5);
+		tree.printTree();
+		System.out.println("test1:"+MostFrequentSum.find(tree))  ; 
+		//use if fail is better than using assert
+ 	}
+
+	@Test
+	public void testFind2() {
+		Tree tree1 = new Tree();
+		tree1.add(0); 
+		tree1.add(22);
+		tree1.add(4);
+		tree1.add();
+		tree1.add();
+		tree1.add(5);
+		tree1.add(6);
+		tree1.add();
+		tree1.add();
+		tree1.add();
+		tree1.add();
+		tree1.add();
+		tree1.add(7);
+		tree1.printTree(); 
+		System.out.println("test2:"+MostFrequentSum.find(tree1))  ; 
+ 	}
+
+}
