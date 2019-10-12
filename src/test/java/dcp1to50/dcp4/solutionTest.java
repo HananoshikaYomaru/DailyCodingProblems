5d4bd6b48bbb29c9dcbbf865aefeeda13e500aa2
Yomaru <mlkyeung@connect.ust.hk>
2019-10-13 00:00:24 +0800

    change a bit of test #194

--- src/test/java/dcp1to50/dcp4/solutionTest.java
+++ src/test/java/dcp1to50/dcp4/solutionTest.java
@@ -9,8 +9,8 @@ public class solutionTest {
 	@Test
 	public void testFirstMissingPositive() {
 		int [ ] array = new int [ ] { -3,-2,0 ,1 ,3, 4 } ; 
-		if(dcp1to50.dcp4.solution.firstMissingPositive(array) !=3)
-			fail("expected is 2 but actual returned is not"); 
+		if(dcp1to50.dcp4.solution.firstMissingPositive(array) !=2)
+			fail("expected is 2 but actual returned is " + dcp1to50.dcp4.solution.firstMissingPositive(array)); 
 	}
 
 }
